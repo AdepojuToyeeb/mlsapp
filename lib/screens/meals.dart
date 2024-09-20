3afab24c30eb2cf55cb05c0e651d96c3b6c8d98d
Adewale Adepoju <adewale.adepoju14@gmail.com>
2024-09-20 21:25:13 +0100

    adding meals data

---
+++ lib/screens/meals.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.meals, required this.title});
+  final List<Meal> meals;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+    );
+  }
+}
