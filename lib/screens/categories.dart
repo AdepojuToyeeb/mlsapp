93405972b848a41b4e45aa4e9bd034f6dd754351
Adewale Adepoju <adewale.adepoju14@gmail.com>
2024-09-20 22:10:56 +0100

    adding navigtion between screens

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const MealsScreen(meals: [], title: "Some"),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +31,12 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           //can use .map, but for in is an alternative
           for (final category in availableCategories)
-            CategoryGridItem(category: category),
+            CategoryGridItem(
+              category: category,
+              onSelect: () {
+                _selectCategory(context);
+              },
+            ),
         ],
       ),
     );
