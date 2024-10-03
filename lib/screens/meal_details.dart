cf31dad54eb44762c6659f5eee7ba074c65eac91
Adewale Adepoju <adewale.adepoju14@gmail.com>
2024-10-03 12:07:54 +0100

    added implicit animation to star icon

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -31,7 +31,23 @@ class MealDetailsScreen extends ConsumerWidget {
                   ),
                 );
               },
-              icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+              icon: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 400),
+                transitionBuilder: (child, animation) {
+                  return SizeTransition(
+                    sizeFactor: Tween(begin: 0.0, end: 1.0).animate(animation),
+                    // turns: Tween<double>(
+                    //   begin: 0.5,
+                    //   end: 1,
+                    // ).animate(animation),
+                    child: child,
+                  );
+                },
+                child: Icon(
+                  isFavorite ? Icons.star : Icons.star_border,
+                  key: ValueKey(isFavorite),
+                ),
+              ),
             ),
           ],
         ),
